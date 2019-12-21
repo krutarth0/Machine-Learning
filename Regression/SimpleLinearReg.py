a9b83368485fd3c165ba32bba578b93427e4dd87
Krutarth Patel <krutarthzero@hotmail.com>
2019-12-21 19:29:03 +0530

    Update SimpleLinearReg.py

--- Regression/SimpleLinearReg.py
+++ Regression/SimpleLinearReg.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset =pd.read_csv('Salary_Data.csv')
+dataset =pd.read_csv('./data/Salary_Data.csv')
 x=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,1:].values
 
