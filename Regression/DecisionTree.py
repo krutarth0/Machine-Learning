9031afcade491d9900c684ee1c41df65df278f83
Krutarth Patel <krutarthzero@hotmail.com>
2019-12-21 19:28:04 +0530

    Update DecisionTree.py

--- Regression/DecisionTree.py
+++ Regression/DecisionTree.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('./data/Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
