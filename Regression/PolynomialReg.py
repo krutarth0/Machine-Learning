2fedee9cfcd646212e152331e03e99c506165b91
Krutarth Patel <krutarthzero@hotmail.com>
2019-12-21 19:28:30 +0530

    Update PolynomialReg.py

--- Regression/PolynomialReg.py
+++ Regression/PolynomialReg.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures 
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('./data/Position_Salaries.csv')
 X = dataset.iloc[:,1:2].values
 y = dataset.iloc[:, 2].values
 
