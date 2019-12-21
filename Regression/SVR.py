6c68010a100788c68c72ae76d21356972e17fda2
Krutarth Patel <krutarthzero@hotmail.com>
2019-12-21 19:23:13 +0530

    Update SVR.py

--- Regression/SVR.py
+++ Regression/SVR.py
@@ -4,6 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -11,14 +13,12 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y.reshape(10,1))
 
 # Fitting SVR to the dataset
-from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
